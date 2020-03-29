8572cd737d2c3306f6fcfeb51576ee3106aea434
liwz11 <liwz_bemyself@163.com>
2020-03-29 21:07:05 +0800

    show bandwidth with echart.js

---
+++ html/mychart.js
@@ -0,0 +1,169 @@
+// https://www.echartsjs.com/examples/zh/editor.html?c=area-rainfall
+
+var time_list = [];
+var ibw_list = [];
+var obw_list = [];
+
+var bw_chart = echarts.init(document.getElementById('bw'));
+
+bw_init_option = {
+	// 标题样式
+    title: {
+        text: '出入带宽',
+        subtext: '',
+        left: 'center',
+        align: 'right'
+    },
+    // 图片位置
+    grid: {
+        bottom: 80
+    },
+    // 工具箱
+    toolbox: {
+        feature: {
+            dataZoom: { yAxisIndex: 'none' },
+            restore: {},
+            saveAsImage: {}
+        },
+        right: 60
+    },
+    // 提示框
+    tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+            type: 'cross',
+            animation: false,
+            label: {
+                backgroundColor: '#505765'
+            }
+        }
+    },
+    // 图例
+    legend: {
+        data: ['出带宽', '入带宽'],
+        left: 60
+    },
+    // 区域缩放
+    dataZoom: [
+        {
+            show: true,
+            realtime: true,
+            start: 80,
+            end: 100
+        },
+        {
+            type: 'inside',
+            realtime: true,
+            start: 80,
+            end: 100
+        }
+    ],
+    // y轴
+    yAxis: [
+        {
+            name: '出带宽(Mbps)',
+            type: 'value',
+            // 横向网格线设置不显示
+            splitLine: { show: false },
+            //max: 1000
+        },
+        {
+            name: '入带宽(Mbps)',
+            nameLocation: 'start',
+            type: 'value',
+            // 横向网格线设置不显示
+            splitLine: { show: false },
+            //max: 1000,
+            inverse: true
+        }
+    ],
+    // 颜色
+    color: ['#EE6363','#4682B4']
+};
+
+
+bw_data_option = {
+	// x轴
+    xAxis: [
+        {
+            type: 'category',
+            // 坐标轴两边留白策略，即x轴坐标开始与结束位置都不在最边缘
+            boundaryGap: false,
+            // 坐标轴线样式
+            axisLine: {onZero: false},
+            data: []
+        }
+    ],
+    series: [
+        {
+            name: '出带宽',
+            type: 'line',
+            //折线平滑
+            smooth : true,
+            animation: true,
+            //折线堆积区域样式
+            areaStyle: {},
+            //线条样式
+            lineStyle: { width: 1 },
+            data: []
+        },
+        {
+            name: '入带宽',
+            type: 'line',
+            //折线平滑
+            smooth : true,
+            yAxisIndex: 1,
+            animation: true,
+            //折线堆积区域样式
+            areaStyle: {},
+            //线条样式
+            lineStyle: { width: 1 },
+            data: []
+        }
+    ]
+};
+
+
+bw_chart.setOption(Object.assign(bw_init_option, bw_data_option));
+
+var t = 0;
+get_data();
+
+function get_data() {
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function () {
+        if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            var res = xmlhttp.responseText;
+            //console.log(res);
+
+            var performance_list = JSON.parse(res);
+            if(performance_list.length > 0) {
+            	t = performance_list[performance_list.length - 1]['t'];
+
+            	performance_list.forEach(function(item) {
+            		time_list.push(item.t);
+					ibw_list.push(item.ibw);
+					obw_list.push(item.obw);
+				});
+
+				bw_data_option.xAxis[0].data = time_list.map(function (str) {
+	                return str.replace(' ', '\n');
+	            });
+
+				bw_data_option.series[0].data = ibw_list;
+				bw_data_option.series[1].data = obw_list;
+
+				bw_chart.setOption(bw_data_option);
+            }
+        }
+    }
+
+    xmlhttp.open("GET", "http://[ADDR]:[PORT]/get_data?t=" + t, true);
+    xmlhttp.timeout = [TIMEOUT] * 1000;
+    xmlhttp.send(null);
+
+    setTimeout(function() {
+        get_data();
+    }, [INTERVAL] * 1000);
+}
--- wtop.py
+++ wtop.py
@@ -3,9 +3,90 @@
 
 
 import os, time, json, re, threading, subprocess
-import socket, fcntl, struct
+import urllib, socket, fcntl, struct
 
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from argparse import ArgumentParser
+
+
+class MyHTTPHandler(BaseHTTPRequestHandler):
+	def setup(self):
+		self.timeout = 2 # avoid request timeout. it works! short timeout as there is only 1 thread
+		BaseHTTPRequestHandler.setup(self)
+	
+	def do_GET(self):
+		root_dir = './html/'
+		temp = self.path.split('?')
+		path = temp[0]
+		pram = ''
+		if len(temp) > 1:
+			pram = urllib.unquote(temp[1])
+
+		if path == '/':
+			path = '/index.html'
+
+		try:
+			content_type = 'text/html'
+			if path.endswith('.js'):
+				content_type = 'application/javascript'
+			elif path.endswith('.css'):
+				content_type = 'text/css'
+
+			if '..' in path:
+				self.send_error(400, 'Bad Request')
+
+			elif path == '/get_data':
+				self.send_response(200)
+				self.send_header('Content-Type', content_type)
+				self.end_headers()
+				
+				t = time.strftime("%Y-%m-%d", time.localtime(time.time())) + ' 00:00:00'
+				temp = pram.split('t=')
+				if len(temp) > 1 and len(temp[1]) == len(t):
+					t = temp[1]
+				date = t.split(' ')[0].replace('-', '')
+
+				res_list = []
+				with open('./logs/%s.log' % date, 'r') as f:
+					for line in f:
+						performance = json.loads(line.strip())
+						if len(res_list) >= 2000:
+							break
+						if performance['t'] <= t:
+							continue
+						res_list.append(performance)
+
+				self.wfile.write(json.dumps(res_list))
+
+			elif path == '/mychart.js':
+				f = open(root_dir + path)
+				self.send_response(200)
+				self.send_header('Content-Type', content_type)
+				self.end_headers()
+
+				global http_conf
+
+				js_text = f.read()
+				js_text = js_text.replace('[ADDR]', http_conf['addr'])
+				js_text = js_text.replace('[PORT]', str(http_conf['port']))
+				js_text = js_text.replace('[INTERVAL]', str(http_conf['interval']))
+				js_text = js_text.replace('[TIMEOUT]', str(http_conf['timeout']))
+				self.wfile.write(js_text)
+				f.close()
+
+			else:
+				f = open(root_dir + path)
+				self.send_response(200)
+				self.send_header('Content-Type', content_type)
+				self.end_headers()
+				self.wfile.write(f.read())
+				f.close()
+
+		except Exception as e:
+			if 'Broken pipe' not in str(e):
+				self.send_error(404, 'File Not Found: %s' % path)
+			else:
+				print('Error: Connection reset by client peer.')
 
 def popen_command(command):
     try:
@@ -20,7 +101,7 @@ def popen_command(command):
 
 
 def monitor(iface, addr):
-	print('[+] monitor the system\'s CPU and memory, the specified interface\'s bandwidth and connections.')
+	print('[+] monitor the system\'s performance.\n')
 
 	byte_cmd = "ifconfig %s" % iface
 	conn_cmd   = "netstat -n | awk '/%s:/ {++S[$NF]} END {for(a in S) print a, S[a]}' | grep 'ESTABLISHED' | cut -d ' ' -f2" % addr
@@ -46,8 +127,8 @@ def monitor(iface, addr):
 		ibytes2 = int(find_bytes[0])
 		obytes2 = int(find_bytes[1])
 
-		ibw = int(round((ibytes2 - ibytes1) * 8 / (t2 - t1))) # bps
-		obw = int(round((obytes2 - obytes1) * 8 / (t2 - t1))) # bps
+		ibw = round((ibytes2 - ibytes1) * 8 / (t2 - t1) / 1000000, 6) # Mbps
+		obw = round((obytes2 - obytes1) * 8 / (t2 - t1) / 1000000, 6) # Mbps
 
 		if conn == '':
 			conn = 0
@@ -56,7 +137,14 @@ def monitor(iface, addr):
 
 		t = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(t2))
 		performance = { 'ibw':ibw, 'obw':obw, 'conn':conn, 't': t }
-		print(performance)
+		
+		if not os.path.exists('./logs'):
+			os.mkdir('./logs')
+
+		date = t.split(' ')[0].replace('-', '')
+		with open('./logs/%s.log' % date, 'a') as f:
+			f.write(json.dumps(performance))
+			f.write('\n')
 
 
 def get_ip_addr(ifname):
@@ -66,17 +154,33 @@ def get_ip_addr(ifname):
 
 
 if __name__ == '__main__':
-	parser = ArgumentParser(description='Capture linux\'s CPU, memory, bandwidth and connections and show in html page, just like the top tool.')
+	parser = ArgumentParser(description='monitor system\' performance and display it in html, just like \'top\'.')
 	parser.add_argument('--iface', default='eth0', help='specify an interface and monitor it, default \'eth0\'')
+	parser.add_argument('--addr', default='127.0.0.1', help='the http server domain or ip addr, default \'127.0.0.1\'')
+	parser.add_argument('--port', default=8888, type=int, help='the tmap server port, default 8888')
+	parser.add_argument('--interval', default=5, type=int, help='the interval to get data in mychart.js, default 5s')
+	parser.add_argument('--timeout', default=5, type=int, help='the timeout to get data in mychart.js, default 5s')
 	args = parser.parse_args()
 
 	iface = args.iface
 	addr = get_ip_addr(iface)
 
+	http_conf = {}
+	http_conf['addr']     = args.addr
+	http_conf['port']     = args.port
+	http_conf['interval'] = args.interval
+	http_conf['timeout']  = args.timeout
+
+	print('')
 	
 	monitor_thread = threading.Thread(target=monitor, args=(iface, addr, ))
 	monitor_thread.setDaemon(True)
 	monitor_thread.start()
-	
-	time.sleep(100)
+
+	try:
+		server = HTTPServer(('127.0.0.1', 8888), MyHTTPHandler)
+		print('[+] start an HTTP server on 127.0.0.1:8888\n')
+		server.serve_forever()
+	except Exception as e:
+		server.socket.close()
 
