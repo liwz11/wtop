00b2b6c0e2dfe811473287edc97128e353831555
liwz11 <liwz_bemyself@163.com>
2020-03-30 23:36:26 +0800

    display connections with mychart.js

--- html/mychart.js
+++ html/mychart.js
@@ -3,8 +3,12 @@
 var time_list = [];
 var ibw_list = [];
 var obw_list = [];
+var conn_list = [];
 
 var bw_chart = echarts.init(document.getElementById('bw'));
+var conn_chart = echarts.init(document.getElementById('conn'));
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 bw_init_option = {
 	// 标题样式
@@ -131,8 +135,113 @@ bw_data_option = {
     ]
 };
 
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+conn_init_option = {
+	// 标题样式
+    title: {
+        text: '连接数',
+        subtext: '',
+        left: 'center',
+        align: 'right'
+    },
+    // 图片位置
+    grid: {
+        bottom: 80
+    },
+    // 工具箱
+    toolbox: {
+        feature: {
+            dataZoom: { yAxisIndex: 'none' },
+            restore: {},
+            saveAsImage: {}
+        },
+        right: 60
+    },
+    // 提示框
+    tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+            type: 'cross',
+            animation: false,
+            label: {
+                backgroundColor: '#505765'
+            }
+        }
+    },
+    // 图例
+    legend: {
+        data: ['连接数'],
+        left: 60
+    }, //*/
+    // 区域缩放
+    dataZoom: [
+        {
+        	show: true,
+            type: 'slider',
+            realtime: true,
+            start: 80,
+            end: 100,
+            showDataShadow: true, //是否显示数据阴影 默认auto
+            showDetail: true
+        },
+        {
+        	show: true,
+            type: 'inside',
+            realtime: true,
+            start: 80,
+            end: 100
+        }//*/
+    ],
+    // y轴
+    yAxis: [
+        {
+            name: '连接数',
+            type: 'value',
+            // 横向网格线设置不显示
+            splitLine: { show: false },
+            max: function(value) {
+            	return value.max + 5
+            },
+        }
+    ],
+    // 颜色
+    color: ['#EE6363','#4682B4']
+};
+
+
+conn_data_option = {
+	// x轴
+    xAxis: [
+        {
+            type: 'category',
+            // 坐标轴两边留白策略，即x轴坐标开始与结束位置都不在最边缘
+            boundaryGap: false,
+            // 坐标轴线样式
+            axisLine: {onZero: false},
+            data: []
+        }
+    ],
+    series: [
+        {
+            name: '连接数',
+            type: 'line',
+            //折线平滑
+            smooth : true,
+            animation: true,
+            //折线堆积区域样式
+            areaStyle: {},
+            //线条样式
+            lineStyle: { width: 1 },
+            data: []
+        }
+    ]
+};
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 bw_chart.setOption(Object.assign(bw_init_option, bw_data_option));
+conn_chart.setOption(Object.assign(conn_init_option, conn_data_option));
 
 var t = 0;
 get_data();
@@ -151,20 +260,25 @@ function get_data() {
 
             	performance_list.forEach(function(item) {
             		time_list.push(item.t);
+            		obw_list.push(item.obw);
 					ibw_list.push(item.ibw);
-					obw_list.push(item.obw);
+					conn_list.push(item.conn);
 				});
 
-            	//bw_data_option.yAxis[0].max = Math.ceil(Math.max.apply(null, obw_list)) + 1;
-            	//bw_data_option.yAxis[1].max = Math.ceil(Math.max.apply(null, ibw_list)) + 1;
 				bw_data_option.xAxis[0].data = time_list.map(function (str) {
 	                return str.replace(' ', '\n');
 	            });
-
 				bw_data_option.series[0].data = obw_list;
 				bw_data_option.series[1].data = ibw_list;
-
 				bw_chart.setOption(bw_data_option);
+
+				conn_data_option.xAxis[0].data = time_list.map(function (str) {
+	                return str.replace(' ', '\n');
+	            });
+				conn_data_option.series[0].data = conn_list;
+				conn_chart.setOption(conn_data_option);
+
+
             }
         }
     }
